ac20069f40420655d250d592d2c96f201b170701
valentyn <valentyn@virtuoso.com>
2014-10-29 22:24:09 +0200

    Added tests for StaffType, Style.

--- src/main/java/com/coffeine/virtuoso/module/user/model/entity/Staff.java
+++ src/main/java/com/coffeine/virtuoso/module/user/model/entity/Staff.java
@@ -17,10 +17,8 @@ package com.coffeine.virtuoso.module.user.model.entity;
 
 import java.io.Serializable;
 import java.util.Calendar;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -28,7 +26,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
