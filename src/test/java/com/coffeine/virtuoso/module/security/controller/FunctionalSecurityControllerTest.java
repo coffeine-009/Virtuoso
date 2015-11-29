d38d32d10b4a658793f141e26458b994041d9244
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2015-11-29 15:48:01 +0200

    Added test for invalid login.

--- src/test/java/com/coffeine/virtuoso/module/security/controller/FunctionalSecurityControllerTest.java
+++ src/test/java/com/coffeine/virtuoso/module/security/controller/FunctionalSecurityControllerTest.java
@@ -84,25 +84,25 @@ public class FunctionalSecurityControllerTest extends AbstractControllerTest {
 
         //- Do Sign Up request -//
         this.mockMvc.perform(
-            post("/security/signup")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                    "{" +
-                        "\"username\": \"unit#test.com\", " +
-                        "\"password\": \"Te$t\", " +
-                        "\"firstName\": \"Unit\", " +
-                        "\"lastName\": \"test\", " +
-                        "\"gender\": false, " +
-                        "\"locale\": \"en-US\", " +
-                        "\"roles\": [" +
-                        "], " +
-                        "\"birthday\": \"1990-08-10\"" +
-                    "}"
-            )
+                post("/security/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                "{" +
+                                        "\"username\": \"unit#test.com\", " +
+                                        "\"password\": \"Te$t\", " +
+                                        "\"firstName\": \"Unit\", " +
+                                        "\"lastName\": \"test\", " +
+                                        "\"gender\": false, " +
+                                        "\"locale\": \"en-US\", " +
+                                        "\"roles\": [" +
+                                        "], " +
+                                        "\"birthday\": \"1990-08-10\"" +
+                                        "}"
+                        )
         ).andDo(print())
             .andExpect( status().isBadRequest() )
-            .andExpect( jsonPath( "$.fieldErrors", notNullValue() ) )
-            .andExpect( jsonPath( "$.fieldErrors", not( empty() ) ) )
+            .andExpect( jsonPath("$.fieldErrors", notNullValue()) )
+            .andExpect(jsonPath("$.fieldErrors", not(empty())))
             ;
         //TODO: finish
     }
@@ -136,7 +136,7 @@ public class FunctionalSecurityControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void testGetAccessToken() throws Exception {
+    public void testSigninSuccess() throws Exception {
 
         //- Success -//
         this.mockMvc.perform(
@@ -165,5 +165,38 @@ public class FunctionalSecurityControllerTest extends AbstractControllerTest {
             .andExpect( jsonPath( "$.expires_in", not( empty() ) ) )
             .andExpect( jsonPath( "$.token_type" ).value( "bearer" ) )
             .andExpect( jsonPath( "$.scope" ).value( "read" ) );
+    }
+
+    @Test
+    public void testSigninFailure() throws Exception {
+
+        //- Success -//
+        this.mockMvc.perform(
+            post( "/oauth/token" )
+                .contentType( MediaType.APPLICATION_JSON )
+                .header(
+                    "Authorization",
+                    "Basic " + new String(
+                        Base64.encodeBase64(
+                            "developer:developer32".getBytes()
+                        )
+                    )
+                )
+                .param( "grant_type", "password" )
+                .param( "scope", "read" )
+                .param( "clientId", "developer" )
+                .param( "clientSecret", "developer32" )
+                .param( "username", "user@virtuoso.com" )
+                .param( "password", "1234" )
+        )
+            .andExpect( status().isBadRequest() )
+            .andExpect( content().contentType( MediaType.APPLICATION_JSON + ";charset=UTF-8" ) )
+            .andExpect( jsonPath( "$.error", notNullValue() ) )
+            .andExpect( jsonPath( "$.error", not( empty() ) ) )
+            .andExpect( jsonPath( "$.error" ).value( "invalid_grant" ) )
+            .andExpect( jsonPath( "$.error_description", notNullValue() ) )
+            .andExpect( jsonPath( "$.error_description", not( empty() ) ) )
+            .andExpect( jsonPath( "$.error_description" ).value( "Bad user credentials" ) );
+        //TODO: finish
     }
 }
