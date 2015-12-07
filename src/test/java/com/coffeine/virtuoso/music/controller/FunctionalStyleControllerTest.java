fb540acb2a86b19fe61866909f25ec1e83eea777
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2015-12-07 23:17:48 +0200

    Prepared classes for putting tests.

--- src/test/java/com/coffeine/virtuoso/music/controller/FunctionalStyleControllerTest.java
+++ src/test/java/com/coffeine/virtuoso/music/controller/FunctionalStyleControllerTest.java
@@ -10,10 +10,16 @@ package com.coffeine.virtuoso.music.controller;
 
 import com.coffeine.virtuoso.module.controller.AbstractRestControllerTest;
 
+import org.junit.Test;
+
 /**
  * Functional tests for StyleController.
  * @see StyleController
  */
 public class FunctionalStyleControllerTest extends AbstractRestControllerTest {
     //TODO: put here tests
+    @Test
+    public void testFilterActionSuccess() throws Exception {
+
+    }
 }
--- src/test/java/com/coffeine/virtuoso/music/controller/StyleControllerTest.java
+++ src/test/java/com/coffeine/virtuoso/music/controller/StyleControllerTest.java
@@ -10,10 +10,16 @@ package com.coffeine.virtuoso.music.controller;
 
 import com.coffeine.virtuoso.module.controller.AbstractRestControllerTest;
 
+import org.junit.Test;
+
 /**
  * Unit tests for StyleController.
  * @see StyleController
  */
 public class StyleControllerTest extends AbstractRestControllerTest {
     //TODO: put here tests
+    @Test
+    public void testFilterActionSuccess() throws Exception {
+
+    }
 }
