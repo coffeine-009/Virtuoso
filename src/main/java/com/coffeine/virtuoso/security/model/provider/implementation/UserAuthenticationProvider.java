a7f93bbdc23f14b483004e24d472bf4275778fc5
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2016-09-18 21:56:46 +0300

    Sign in via social improvements.

--- src/main/java/com/coffeine/virtuoso/security/model/provider/implementation/UserAuthenticationProvider.java
+++ src/main/java/com/coffeine/virtuoso/security/model/provider/implementation/UserAuthenticationProvider.java
@@ -28,6 +28,9 @@ import java.util.List;
 import java.util.Map;
 import javax.transaction.Transactional;
 
+import static org.springframework.util.Assert.isTrue;
+import static org.springframework.util.Assert.notNull;
+
 /**
  * Authentication provider of users.
  *
@@ -64,30 +67,39 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
         Authentication authentication
     ) throws AuthenticationException {
 
-        final Map<String, String> details = (Map<String, String>) authentication.getDetails();
-        final String socialToken = details.get( "social_token" );
+        try {
+            final Map<String, String> details = (Map<String, String>) authentication.getDetails();
+            final String socialToken = details.get( "social_token" );
+            final String userId = details.get( "user_id" );
+            final String expiresIn = details.get( "expires_in" );
+            final boolean isSocialSignIn = !StringUtils.isEmpty( socialToken );
 
-        //- Search user -//
-        final boolean isSocialSignIn = !StringUtils.isEmpty( socialToken );
-        User user = isSocialSignIn
-            ? this.userService.findBySocialId( Long.parseLong( details.get( "user_id" ) ) )
-            : this.userService.findByUsernameAndPassword(
-                "" + authentication.getPrincipal(),
-                this.passwordEncoder.encodePassword(
-                    "" + authentication.getCredentials(),
-                    null
-                )
+            //- Check params -//
+            isTrue(
+                (isSocialSignIn && !StringUtils.isEmpty( userId ) && !StringUtils.isEmpty( expiresIn ))
+                || !isSocialSignIn
             );
 
-        if ( user != null ) {
+            //- Search user -//
+            User user = isSocialSignIn
+                ? this.userService.findBySocialId( Long.parseLong( userId ) )
+                : this.userService.findByUsernameAndPassword(
+                    "" + authentication.getPrincipal(),
+                    this.passwordEncoder.encodePassword(
+                        "" + authentication.getCredentials(),
+                        null
+                    )
+                );
+
+            //- Check if user exists -//
+            notNull( user );
+
             //- Update acces token -//
             if ( isSocialSignIn ) {
-                final Integer expiresIn = Integer.parseInt( details.get( "expires_in" ) );
-
                 final SocialAccount socialAccount = user.getSocialAccounts().iterator().next();
                 //- Update access params -//
                 socialAccount.setAccessToken( socialToken );
-                socialAccount.setExpiresIn( expiresIn );
+                socialAccount.setExpiresIn( Integer.parseInt( expiresIn ) );
 
                 this.userService.update( user );
             }
@@ -96,7 +108,7 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
             List<GrantedAuthority> authorities = new ArrayList<>();
 
             //- Populate roles into authentication object -//
-            user.getRoles().forEach( 
+            user.getRoles().forEach(
                 role -> authorities.add(
                     new SimpleGrantedAuthority(
                         role.getCode()
@@ -112,8 +124,8 @@ public class UserAuthenticationProvider implements AuthenticationProvider {
             );
 
             return authToken;
-        } else {
-            throw new BadCredentialsException( "Bad user credentials" );
+        } catch ( IllegalArgumentException e ) {
+            throw new BadCredentialsException( "Bad user credentials", e );
         }
     }
 
