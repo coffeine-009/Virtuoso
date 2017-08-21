b8a041990daf7552197574362df282a96627586e
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-08-22 00:19:42 +0300

    Disable Resource server.

--- src/main/java/com/thecoffeine/virtuoso/VirtuosoApplication.java
+++ src/main/java/com/thecoffeine/virtuoso/VirtuosoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * Virtuoso application.
@@ -14,7 +13,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
  * @version 1.0
  */
 @SpringBootApplication
-@EnableGlobalMethodSecurity( prePostEnabled = true )
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableEurekaClient
