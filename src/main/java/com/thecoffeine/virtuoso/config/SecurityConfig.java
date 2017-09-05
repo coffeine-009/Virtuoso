7af11aaa8d239ba17744b3374e7c86204c662de0
Vitaliy Tsutsman <vitaliyacm@gmail.com>
2017-09-05 10:09:49 +0300

    Make song public.

--- src/main/java/com/thecoffeine/virtuoso/config/SecurityConfig.java
+++ src/main/java/com/thecoffeine/virtuoso/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.thecoffeine.virtuoso.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * Resource server configuration.
@@ -12,19 +14,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @Profile( "default" )
 @Configuration
 @EnableResourceServer
-public class SecurityConfig {//} extends ResourceServerConfigurerAdapter {
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
-//    @Override
-//    public void configure( HttpSecurity http ) throws Exception {
-//        http.csrf().disable();
-//        http.httpBasic().disable();
-//
-//        http.requestMatchers().antMatchers( "/music/**" )
-//            .and()
-//            .authorizeRequests()
-//            .antMatchers( "/music/songs**" ).permitAll()
-//            .anyRequest().authenticated();
-//
-//        http.sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS);
-//    }
+    @Override
+    public void configure( HttpSecurity http ) throws Exception {
+        http.csrf().disable();
+        http.httpBasic().disable();
+
+        http.requestMatchers().antMatchers( "/music/**" )
+            .and()
+            .authorizeRequests()
+            .antMatchers( "/music/songs/*" ).permitAll()
+            .anyRequest().authenticated();
+    }
 }
